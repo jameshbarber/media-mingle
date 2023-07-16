63a51dc577c65707f85b043e668c492ad83a2cec
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-16 23:08:55 +0200

    Switch explicit rendering of <Home/> with dynamic <App/>
    This runs the code in "App.js" which checks the route and renders conditionally

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Home from './Home';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./global.css";
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Home />
+    <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
