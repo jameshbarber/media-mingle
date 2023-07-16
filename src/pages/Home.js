4fa6d6f5f7d60148a5a067893bb56ff2990ddfbc
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-16 23:26:27 +0200

    Swap order of DOM absolute components (readability)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,14 @@ const Home = () => {
 
   return (
     <section className={styles.home}>
-      <SpotifySearchCard />
-      <IMDbCard />
       <div className={styles.mediamingleParent}>
         <b className={styles.mediamingle}>MediaMingle</b>
         <b className={styles.anAppThat}>
           An app that lets you search for music, tweets AND movies!
         </b>
       </div>
+      <SpotifySearchCard />
+      <IMDbCard />
     </section>
   );
 };
