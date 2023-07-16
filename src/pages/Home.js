0c67d8ff5036ddff5d412a275b05e554231ddaa2
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-17 00:18:47 +0200

    Add handler functions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import Layout from "../layouts";
 import { Input } from "@geist-ui/core";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  //submit the title entered
+  const handleInput = e => {
+    setName(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/movie-results?name=${name}`)
+  };
+
   return (
     <Layout>
-      <Input placeholder="Search movies & music"/>
+      <form onSubmit={handleSubmit}>
+        <Input placeholder="Search movies & music" onChange={handleInput} />
+      </form>
     </Layout>
   );
 };
