a619ae359bb043bf06e9a206844f3290a42614db
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-17 01:52:01 +0200

    Add Loading Skeleton

--- src/components/ResultListItem/index.js
+++ src/components/ResultListItem/index.js
@@ -1,6 +1,12 @@
 import styles from "./ResultListItem.module.css"
+import Skeleton from "react-loading-skeleton"
+import 'react-loading-skeleton/dist/skeleton.css'
+
 
 const ResultListItem = ({ title, subtitle, image }) => {
+
+    if (!title && !subtitle && !image) return <Skeleton height={400}/>
+
     return <div className={styles.item} style={{
         backgroundImage: `url(${image})`
     }}>
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -4,40 +4,62 @@ import Layout from "../layouts";
 import SearchField from "../components/SearchField";
 import Searcher from "../services/combined-search";
 import ResultListItem from "../components/ResultListItem";
-import { Grid } from "@geist-ui/core";
 
 const getMovieName = () => {
   let params = new URLSearchParams(window.location.search);
   return params.get("name");
 }
 
+const ResultContainer = ({ children }) => {
+  return <div style={{
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+    gap: "10px"
+  }}>
+    {children}
+  </div>
+}
+
+const Loader = () => {
+  return <ResultContainer>{[1, 2, 3, 4, 5, 6].map(i => {
+    return <ResultListItem key={i} />
+  })}</ResultContainer>
+}
+
+const MovieResults = ({ movies, loading }) => {
+
+  if (loading) return <Loader />
+
+  return <div>
+    Movie Results
+    <ResultContainer>
+      {movies?.map((movie, i) => {
+        const { Poster, Title } = movie
+        return <ResultListItem key={i} title={Title} image={Poster} />
+      })}
+    </ResultContainer>
+  </div>
+}
+
+
 const Results = () => {
+  const [loading, setLoading] = useState()
   const searchedName = getMovieName();
   const [searchResults, setSearchResults] = useState()
 
   useEffect(() => {
-    Searcher.search(searchedName).then((res => setSearchResults(res)))
-  })
+    setLoading(true)
+    Searcher.search(searchedName).then((res => {
+      setSearchResults(res)
+      setLoading(false)
+    }))
 
-  if (!searchResults) return "Loading..."
-
-  const { movies, songs } = searchResults
+  }, [searchedName])
 
   return (
     <Layout>
       <SearchField value={searchedName} />
-      Movie Results
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-        gap: "10px"
-      }}>
-        {movies?.map((movie, i) => {
-          const { Poster, Title } = movie
-          return <ResultListItem key={i} title={Title} image={Poster} />
-        })}
-      </div>
-      Song Results
+      <MovieResults loading={loading} movies={searchResults?.movies} />
     </Layout>
   );
 }
