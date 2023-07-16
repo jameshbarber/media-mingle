cab30412f2a44ee2b69306c23e4b9b1b92388c9e
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-16 23:56:33 +0200

    Use Geist Grid to make it responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import SpotifySearchCard from "../components/SpotifySearchCard";
 import IMDbCard from "../components/IMDbCard";
 import Layout from "../layouts";
+import { Grid } from "@geist-ui/core";
 
 const Home = () => {
   return (
     <Layout>
-      <SpotifySearchCard />
-      <IMDbCard />
+      <Grid.Container>
+        <Grid xs={24} md={12}>
+          <SpotifySearchCard />
+        </Grid>
+        <Grid xs={24} md={12}>
+          <IMDbCard />
+        </Grid>
+      </Grid.Container>
     </Layout>
   );
 };
