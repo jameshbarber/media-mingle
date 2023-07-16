ffeeaa3417544aaaa2b8aa3f476ceafb0ceddea1
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-17 00:41:57 +0200

    Use new Searcher service

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,15 +1,8 @@
 // MovieResults.js
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Layout from "../layouts";
-
-const api_url = "http://www.omdbapi.com/?i=tt3896198&apikey=caa27cea";
-
-//get response from API
-const getMovies = async (name) => {
-  const res = await axios.get(`${api_url}&s=${name}&type=movie&page=1`)
-  return res?.data?.Search;
-};
+import SearchField from "../components/SearchField";
+import Searcher from "../services/combined-search";
 
 const getMovieName = () => {
   let params = new URLSearchParams(window.location.search);
@@ -18,29 +11,28 @@ const getMovieName = () => {
 
 const Results = () => {
   const searchedName = getMovieName();
-  const [movies, setMovies] = useState()
+  const [searchResults, setSearchResults] = useState()
 
   useEffect(() => {
-    getMovies(searchedName).then((res) => {
-      setMovies(res);
-    });
+    Searcher.search(searchedName).then((res => setSearchResults(res)))
   })
+
+  if (!searchResults) return "Loading..."
+
+  const { movies, songs } = searchResults
 
   return (
     <Layout>
-      {movies ? <div className="movies">
-        {movies?.map((movie) => (
-          <div key={movie.imdbID} className="movie">
-            <img src={movie.Poster} alt="" />
-            <div className="movie-title">
-              <p>{movie.Title}</p>
-            </div>
-            <button className="movie-detailsBtn">Details</button>
+      <SearchField value={searchedName} />
+      {movies?.map((movie) => (
+        <div key={movie.imdbID} className="movie">
+          <img src={movie.Poster} alt="" />
+          <div className="movie-title">
+            <p>{movie.Title}</p>
           </div>
-        ))}
-      </div> : <div>
-        Loading...
-      </div>}
+          <button className="movie-detailsBtn">Details</button>
+        </div>
+      ))}
     </Layout>
   );
 }
