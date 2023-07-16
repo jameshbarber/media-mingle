c30f675caee1b865219933724833c48216eb7ca8
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-17 01:12:29 +0200

    Style ResultListItem

--- src/components/ResultListItem/index.js
+++ src/components/ResultListItem/index.js
@@ -1,13 +1,17 @@
 import styles from "./ResultListItem.module.css"
 
 const ResultListItem = ({ title, subtitle, image }) => {
-    return <div className={styles.item}>
-        <img style={styles.image} src={image} alt="" />
-        <div className={styles.titleContainer}>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.subtitle}>{subtitle}</div>
+    return <div className={styles.item} style={{
+        backgroundImage: `url(${image})`
+    }}>
+        <div className={styles.overlay}>
+            <div className={styles.titleContainer}>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.subtitle}>{subtitle}</div>
+            </div>
         </div>
     </div>
+
 }
 
 export default ResultListItem
