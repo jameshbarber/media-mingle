c3da0957176c108cae17f5ae3ce915e86507136e
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-17 00:16:23 +0200

    Remove cards & replace with unified search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,10 @@
-import SpotifySearchCard from "../components/SpotifySearchCard";
-import IMDbCard from "../components/IMDbCard";
 import Layout from "../layouts";
-import { Grid } from "@geist-ui/core";
+import { Input } from "@geist-ui/core";
 
 const Home = () => {
   return (
     <Layout>
-      <Grid.Container>
-        <Grid xs={24} md={12}>
-          <SpotifySearchCard />
-        </Grid>
-        <Grid xs={24} md={12}>
-          <IMDbCard />
-        </Grid>
-      </Grid.Container>
+      <Input placeholder="Search movies & music"/>
     </Layout>
   );
 };
