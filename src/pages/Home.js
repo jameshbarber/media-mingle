968e199eaff6265c241dc2bc952322607b78dbff
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-16 23:35:37 +0200

    Extract title to own component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,27 @@
 import SpotifySearchCard from "../components/SpotifySearchCard";
 import IMDbCard from "../components/IMDbCard";
 import styles from "./Home.module.css";
+import Layout from "../layouts";
+
+const TitleSection = () => {
+  return <div className={styles.mediamingleParent}>
+    <b className={styles.mediamingle}>MediaMingle</b>
+    <b className={styles.anAppThat}>
+      An app that lets you search for music, tweets AND movies!
+    </b>
+  </div>
+}
 
 const Home = () => {
 
   return (
-    <section className={styles.home}>
-      <div className={styles.mediamingleParent}>
-        <b className={styles.mediamingle}>MediaMingle</b>
-        <b className={styles.anAppThat}>
-          An app that lets you search for music, tweets AND movies!
-        </b>
-      </div>
-      <SpotifySearchCard />
-      <IMDbCard />
-    </section>
+    <Layout>
+      <section className={styles.home}>
+        <TitleSection />
+        <SpotifySearchCard />
+        <IMDbCard />
+      </section>
+    </Layout>
   );
 };
 
