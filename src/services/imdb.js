a7bf1cd5a2d99fea1a738086ad8820bc5a50b440
jameshbarber <7759481+jameshbarber@users.noreply.github.com>
2023-07-17 12:31:07 +0200

    Switch IMDBApi to Axios (consistency)

--- src/services/imdb.js
+++ src/services/imdb.js
@@ -1,10 +1,14 @@
+import axios from "axios";
+
 class IMDBApi {
     static baseURL = "http://www.omdbapi.com/?i=tt3896198&apikey=caa27cea";
+    
     static async search(id) {
-        const response = await fetch(`${IMDBApi.baseURL}&s=${id}&type=movie&page=1`);
-        const data = await response.json();
-        return data.Search;
+        const response = await axios.get(`${IMDBApi.baseURL}&s=${id}&type=movie&page=1`);
+        const data = response?.data
+        return data?.Search;
     }
 }
+
 
 export default IMDBApi
